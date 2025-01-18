f823fe89f365bcd4b61e3ce7626d92783acdd26f
Purna <purna.csahu24@gmail.com>
2025-01-18 14:04:43 +0530

    Commit by Purna-Added One Printing Statement

--- LearningSelenium/src/test/java/NewPackage/DemoTest.java
+++ LearningSelenium/src/test/java/NewPackage/DemoTest.java
@@ -3,9 +3,10 @@ package NewPackage;
 import org.testng.annotations.Test;
 
 public class DemoTest {
-@Test
-public void demo() {
-	System.out.println("Harry");
-	System.out.println("Rangareddy");
-}
+	@Test
+	public void demo() {
+		System.out.println("Harry");
+		System.out.println("Rangareddy");
+		System.out.println("Purna");
+	}
 }
