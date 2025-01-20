3414523420c2d5acbf1d85786141e498ce33dec4
NAGARAJU <NAGARAJU@LAPTOP-PVV7N6N8>
2025-01-20 17:59:14 +0530

    Added a New SOP Statement

--- LearningSelenium/src/test/java/NewPackage/DemoTest.java
+++ LearningSelenium/src/test/java/NewPackage/DemoTest.java
@@ -16,6 +16,7 @@ public class DemoTest {
 		System.out.println("Purna");
 		System.out.println("Jaheer");
 		System.out.println("Interview bokka");
+		System.out.println("For Test Purpose");
 
 	}
 
